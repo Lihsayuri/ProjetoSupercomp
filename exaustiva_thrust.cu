f3e11bc70a0aee11ce4ada4b618f9a3ed7bec797
Lihsayuri <liviasm1@al.insper.edu.br>
2023-05-21 15:26:54 -0300

    Socorro

--- exaustiva_thrust.cu
+++ exaustiva_thrust.cu
@@ -14,7 +14,7 @@
 #include <ctime>
 #include <omp.h>
  // imports do thrust
-#include <thrust/host_vector.h>
+#include <thrust/host_vector.h>0000000
 #include <thrust/device_vector.h>
 #include <thrust/generate.h>
 #include <thrust/functional.h>
@@ -104,7 +104,46 @@ int main(){
     cout << max_j << endl;
     cout << dp[(N-1 * (M)) + max_j] << endl;
 
+    for (int i = 0; i < N*M; i++) {
+        cout << dp[i] << " ";
+    }
+
 
 
     return 0;
 }
+
+
+
+// O primeiro loop `for` no código está preenchendo a matriz `dp` com as soluções para subproblemas menores. Ele itera sobre os valores de `i` de 0 a N-1 e `j` de 0 a M-1, representando as posições da matriz `dp`.
+
+// Dentro desse loop, ele calcula o número máximo de filmes que podem ser assistidos até o filme i e categoria j. Isso é feito comparando as informações dos filmes anteriores (de 0 a i-1) e verificando se as condições são atendidas para adicionar um novo filme.
+
+// A condição para adicionar um novo filme é a seguinte:
+// - A categoria do filme k (com k de 0 a i-1) é igual a j.
+// - O horário de término do filme k é menor ou igual ao horário de início do filme i.
+// - O número total de filmes na categoria j até o momento, representado por `dp[(k*(M)) + j]`, mais 1, é menor ou igual ao número máximo de filmes permitidos na categoria j, representado por `filmes_por_categoria_gpu[j]`.
+
+// Se todas essas condições forem atendidas, o número máximo de filmes é atualizado para `dp[(k*(M)) + j] + 1`. Caso contrário, o número máximo de filmes permanece o mesmo, `dp[(k*(M)) + j]`.
+
+// No final do primeiro loop `for`, a matriz `dp` estará preenchida com as soluções ótimas para cada subproblema, ou seja, o número máximo de filmes que podem ser assistidos até cada posição da matriz.
+
+// Após o primeiro loop `for`, o código encontra o número máximo de filmes que podem ser assistidos verificando o valor mais alto na última linha da matriz `dp`, correspondente aos filmes N-1 e todas as categorias.
+
+// Por fim, o código imprime o número máximo de filmes e também imprime a matriz `dp` para fins de depuração.
+
+// Desculpe pela confusão anterior. Houve um erro de interpretação no código. Vamos esclarecer o significado dos elementos da matriz e como encontrar o número máximo de filmes que podem ser assistidos em um dia.
+
+// No código fornecido, a matriz `dp` não representa o número máximo de filmes que podem ser assistidos para cada categoria. Na verdade, a matriz `dp` é usada para calcular a quantidade máxima de filmes que podem ser assistidos considerando todas as combinações possíveis de filmes e categorias até um determinado ponto.
+
+// Cada elemento `dp[i*(M) + j]` da matriz `dp` representa o número máximo de filmes que podem ser assistidos considerando os primeiros `i` filmes e as primeiras `j` categorias. Portanto, o valor em `dp[N*(M) + j]` representa o número máximo de filmes que podem ser assistidos considerando todos os filmes e a categoria `j`.
+
+// Para encontrar o número máximo de filmes que podem ser assistidos em um dia, considerando todas as categorias, é necessário encontrar o maior valor entre `dp[N*(M) + j]` para `j` variando de 0 a M-1.
+
+// Em relação aos horários, o código já realiza a verificação das condições `end_times[k] <= start_times[i]` para garantir que os horários não se sobreponham. Portanto, os filmes só serão considerados se o horário de término do filme anterior for menor ou igual ao horário de início do filme atual.
+
+// No entanto, o código atual não considera a restrição de não assistir mais de um filme ao mesmo tempo. Se você deseja garantir que os horários não estejam se sobrepondo, será necessário modificar o código para levar em conta essa restrição adicional.
+
+// Uma abordagem possível seria usar programação dinâmica com uma matriz tridimensional, onde o terceiro índice representaria o horário de início. Cada elemento `dp[i][j][k]` representaria o número máximo de filmes que podem ser assistidos considerando os primeiros `i` filmes, as primeiras `j` categorias e o horário de início `k`. Você teria que ajustar o código para essa nova estrutura e considerar as restrições de horário ao calcular as soluções.
+
+// Espero que isso esclareça a sua dúvida. Se precisar de mais informações, fique à vontade para perguntar!
